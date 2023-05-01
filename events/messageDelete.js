e26db4192babe6e8a60cb2338ee33536fa52b205
Peally <pieclandre@gmail.com>
2023-05-01 14:05:21 +0200

    feat : createLogChannel if does not exist

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,26 +1,32 @@
-const {Events} = require("discord.js");
-const {embedConstructor} = require("../components/Embed");
+const { Events } = require("discord.js");
+const { embedConstructor } = require("../components/Embed");
+const { createLogChannel } = require("../createLogChannel.js");
 
 module.exports = {
-    name: Events.MessageDelete,
-    timeout: 5000,
-    async execute(message) {
-        message.guild
-            .fetchAuditLogs()
-            .then((audit) =>
-                message.guild.channels.fetch()
-                    .then((channels) =>
-                        channels.find((channel) => channel.name === "logs").send(
-                            {
-                                embeds: [embedConstructor(
-                                    "messageDelete",
-                                    `${message.client.user.tag}`,
-                                    `${audit.entries.first().executor.username}`,
-                                    `${message.author.username}`,
-                                    `${message}`)
-                                ]
-                            }
-                        )))
-            .catch(err => console.log(err));
-    },
+  name: Events.MessageDelete,
+  timeout: 5000,
+  async execute(message) {
+    createLogChannel(message);
+
+    message.guild
+      .fetchAuditLogs()
+      .then((audit) =>
+        message.guild.channels.fetch().then((channels) =>
+          channels
+            .find((channel) => channel.name === "logs")
+            .send({
+              embeds: [
+                embedConstructor(
+                  "messageDelete",
+                  `${message.client.user.tag}`,
+                  `${audit.entries.first().executor.username}`,
+                  `${message.author.username}`,
+                  `${message}`
+                ),
+              ],
+            })
+        )
+      )
+      .catch((err) => console.log(err));
+  },
 };
