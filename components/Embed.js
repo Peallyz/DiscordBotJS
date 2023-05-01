140d67d500ef6a202f2c4a39374cc08634972941
Peally <pieclandre@gmail.com>
2023-05-01 20:06:07 +0200

    Update .env with dev ID

--- components/Embed.js
+++ components/Embed.js
@@ -1,34 +1,40 @@
 // at the top of your file
-const {EmbedBuilder} = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
-function embedConstructor(
-    type,
-    content
-) {
+function embedConstructor(type, content) {
+  switch (type) {
+    case "messageDelete": {
+      const anthyra = content.guild.members.cache.find(
+        (user) => user.id === process.env.ANTHYRA_ID
+      ).user;
+      const peally = content.guild.members.cache.find(
+        (test) => test.id === process.env.PEALLY_ID
+      ).user;
 
-    switch (type) {
-        case "messageDelete": {
-            const anthyra = content.guild.members.cache.find(user => user.id === '819175877816352780').user;
-            const peally = content.guild.members.cache.find(test => test.id === '290083594586554370').user;
-
-            // inside a command, event listener, etc.
-            return new EmbedBuilder()
-                .setColor("Red")
-                .setTitle(`Suppression d'un message dans #${content.guild.channels.cache.get(content.channelId).name}`)
-                .setAuthor({
-                    name: content.author.tag,
-                    iconURL: content.author.avatarURL(),
-                    url: "https://discord.js.org",
-                })
-                .setDescription(`${content}`)
-                .setThumbnail(content.guild.iconURL())
-                .setTimestamp()
-                .setFooter({
-                    text: `Bot réalisé par ${anthyra ? anthyra.tag : "Anthyra"} et ${peally ? peally.tag : "Peally"}`,
-                    iconURL: content.guild.client.user.avatarURL(),
-                });
-        }
+      // inside a command, event listener, etc.
+      return new EmbedBuilder()
+        .setColor("Red")
+        .setTitle(
+          `Suppression d'un message dans #${
+            content.guild.channels.cache.get(content.channelId).name
+          }`
+        )
+        .setAuthor({
+          name: content.author.tag,
+          iconURL: content.author.avatarURL(),
+          url: "https://discord.js.org",
+        })
+        .setDescription(`${content}`)
+        .setThumbnail(content.guild.iconURL())
+        .setTimestamp()
+        .setFooter({
+          text: `Bot réalisé par ${anthyra ? anthyra.tag : "Anthyra"} et ${
+            peally ? peally.tag : "Peally"
+          }`,
+          iconURL: content.guild.client.user.avatarURL(),
+        });
     }
+  }
 }
 
-module.exports = {embedConstructor};
+module.exports = { embedConstructor };
