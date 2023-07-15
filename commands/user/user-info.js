9da38cf42281b2141839320b96b8a98c224e4ee6
Peally <pieclandre@gmail.com>
2023-07-15 23:41:25 +0200

    clean : remove console.log and fix typo error on setDescription

--- commands/user/user-info.js
+++ commands/user/user-info.js
@@ -1,44 +1,46 @@
-const {SlashCommandBuilder, EmbedBuilder} = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("user-info")
-        .setDescription("Répond avec Pong !"),
-    async execute(interaction) {
-        let options = {year: 'numeric', month: 'long', day: 'numeric'};
-        console.log(interaction.client.user.avatarURL());
+  data: new SlashCommandBuilder()
+    .setName("user-info")
+    .setDescription("Genère un embed avec les informations de l'utilisateur"),
+  async execute(interaction) {
+    let options = { year: "numeric", month: "long", day: "numeric" };
 
-        const embed = new EmbedBuilder()
-            .setColor("#0099ff")
-            .setAuthor(
-                {
-                    name: interaction.client.user.username,
-                    iconURL: interaction.client.user.avatarURL()
-                })
-            .setTitle("User informations")
-            .setDescription(`Informations about ${interaction.user.username}`)
-            .addFields(
-                {
-                    name: 'Name',
-                    value: `${interaction.user.username}`,
-                    inline: true
-                },
-                {
-                    name: 'User ID',
-                    value: `${interaction.user.id}`,
-                    inline: true
-                },
-                {
-                    name: 'Creation Date',
-                    value: `${interaction.user.createdAt.toLocaleString('fr-FR', options)}`,
-                    inline: true
-                },
-            )
-            .setImage(interaction.user.avatarURL({dynamic: true, size: 512}))
-            .setFooter({text: 'V.0.1', iconURL: interaction.client.user.avatarURL()})
-            .setTimestamp();
-        await interaction.reply(
-            {embeds: [embed]}
-        );
-    },
+    const embed = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setAuthor({
+        name: interaction.client.user.username,
+        iconURL: interaction.client.user.avatarURL(),
+      })
+      .setTitle("User informations")
+      .setDescription(`Informations about ${interaction.user.username}`)
+      .addFields(
+        {
+          name: "Name",
+          value: `${interaction.user.username}`,
+          inline: true,
+        },
+        {
+          name: "User ID",
+          value: `${interaction.user.id}`,
+          inline: true,
+        },
+        {
+          name: "Creation Date",
+          value: `${interaction.user.createdAt.toLocaleString(
+            "fr-FR",
+            options
+          )}`,
+          inline: true,
+        }
+      )
+      .setImage(interaction.user.avatarURL({ dynamic: true, size: 512 }))
+      .setFooter({
+        text: "V.0.1",
+        iconURL: interaction.client.user.avatarURL(),
+      })
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+  },
 };
